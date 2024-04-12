5d01efb2f7e69403f0dec19f18b0c6c734c256c3
Justin <16kimj97@gmail.com>
2024-04-12 15:34:09 -0700

    all forms done

--- app/forms/__init__.py
+++ app/forms/__init__.py
@@ -1,11 +1,7 @@
 from .login_form import LoginForm
 from .signup_form import SignUpForm
-<<<<<<< HEAD
 from .comment_form import AddCommentForm
 from .playlist_form import PlaylistForm
 from .song_form import AddSongForm
-=======
-from. song_form import AddSongForm
-from. add_to_playlist import AddSongToPlaylistForm
-from .playlist_form import PlaylistForm
->>>>>>> playlist-form
+from .edit_comment import EditCommentForm
+from .add_to_playlist import AddSongToPlaylistForm
