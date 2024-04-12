0b5353729f1417b61dcbf52c3da7e397d061dbc4
Thanh Pham <jonathanphamw22@gmail.com>
2024-04-12 11:21:57 -0700

    Finish comments table

---
+++ app/models/comments.py
@@ -0,0 +1,19 @@
+from .db import db, environment, SCHEMA, add_prefix_for_prod
+from datetime import datetime
+
+class Comment(db.Model):
+    __tablename__ = "comments"
+
+    if environment == "production":
+        __table_args__ = {'schema': SCHEMA}
+
+    id = db.Column(db.Integer, primary_key=True)
+    song_id = db.Column(db.Integer, db.ForeignKey(add_prefix_for_prod("songs.id")), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey(add_prefix_for_prod("users.id")), nullable=False)
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
+    body = db.Column(db.Text)
+
+    song = db.relationship("Song", back_populates="comments")
+
+    user = db.relationship("User", back_populate="comments")
+
