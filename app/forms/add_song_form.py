564eb6a5f7aa7abd288eff4ee94d01e89632267e
Justin <16kimj97@gmail.com>
2024-04-12 14:50:35 -0700

    removed user from add-song

--- app/forms/add_song_form.py
+++ app/forms/add_song_form.py
@@ -1,8 +1,6 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, DateField, SubmitField
 from wtforms.validators import DataRequired, ValidationError
-from app.models import User
-from flask_login import current_user
 
 
 class AddSongForm(FlaskForm):
