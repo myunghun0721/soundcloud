b16e02d00ff133c772e132c5d4698151eb6e505e
myunghun choi <myunghun0721@gmail.com>
2024-04-16 09:22:28 -0700

    routes done

--- react-vite/src/components/Navigation/Navigation.jsx
+++ react-vite/src/components/Navigation/Navigation.jsx
@@ -1,19 +1,28 @@
 import { NavLink } from "react-router-dom";
 import ProfileButton from "./ProfileButton";
+import { useSelector} from "react-redux"
 import "./Navigation.css";
 
 function Navigation() {
-  return (
-    <ul>
-      <li>
-        <NavLink to="/">Home</NavLink>
-      </li>
+  const sessionUser = useSelector(state => state.session.user);
+  console.log("🚀 ~ Navigation ~ sessionUser:", sessionUser)
 
-      <li>
-        <ProfileButton />
-      </li>
-    </ul>
+  return (
+    <div className='div-navigation-wrapper'>
+      <div className='div-navigation-a'>
+        <NavLink to="/">
+          <img className='homeImg'src='../../../public/favicon.ico' alt='Home img'></img>
+        </NavLink>
+      </div>
+
+      <div className='div-navigation-a-user'>
+        {/* {sessionUser && <NavLink to="/spots/new" className={`a-createSpot`}>Create a New Spot</NavLink>} */}
+        <ProfileButton user={sessionUser} />
+      </div>
+
+    </div>
   );
 }
+
 
 export default Navigation;
