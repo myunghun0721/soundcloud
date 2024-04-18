0806c7b74def047b57e10b7c4edeb8ed78e60ab0
Justin <16kimj97@gmail.com>
2024-04-18 08:47:00 -0700

    connected profilebutton to profile

--- react-vite/src/components/Navigation/ProfileButton.jsx
+++ react-vite/src/components/Navigation/ProfileButton.jsx
@@ -44,6 +44,7 @@ function ProfileButton() {
     closeMenu();
   };
 
+
   return (
     <>
       <button className="nav-user-button" onClick={toggleMenu}>
@@ -53,7 +54,7 @@ function ProfileButton() {
         <ul className={"profile-dropdown"} ref={ulRef}>
           {user ? (
             <>
-              <NavLink to='/'><li><CgProfile /> Profile</li></NavLink>
+              <NavLink to="/user/current"><li><CgProfile /> Profile</li></NavLink>
               <NavLink to='/'><li><LuMusic2 /> Songs</li></NavLink>
               <NavLink to='/'><li><FaHeart /> Likes</li></NavLink>
               <NavLink to='/'><li><PiPlaylistFill /> Playlists</li></NavLink>
