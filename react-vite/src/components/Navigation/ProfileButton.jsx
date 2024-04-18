7d5f4a23bf7272729695bd998a9dfb9fd3ea5300
myunghun choi <myunghun0721@gmail.com>
2024-04-18 15:23:52 -0700

    main

--- react-vite/src/components/Navigation/ProfileButton.jsx
+++ react-vite/src/components/Navigation/ProfileButton.jsx
@@ -53,10 +53,10 @@ function ProfileButton() {
         <ul className={"profile-dropdown"} ref={ulRef}>
           {user ? (
             <>
-              <NavLink to='/'><li><CgProfile /> Profile</li></NavLink>
-              <NavLink to='/'><li><LuMusic2 /> Songs</li></NavLink>
-              <NavLink to='/'><li><FaHeart /> Likes</li></NavLink>
-              <NavLink to='/'><li><PiPlaylistFill /> Playlists</li></NavLink>
+              <NavLink to='/user/current'><li><CgProfile /> Profile</li></NavLink>
+              {/* <NavLink to='/'><li><LuMusic2 /> Songs</li></NavLink> */}
+              <NavLink to='/user/current/likes'><li><FaHeart /> Likes</li></NavLink>
+              {/* <NavLink to='/'><li><PiPlaylistFill /> Playlists</li></NavLink> */}
               <hr></hr>
               <li>
                 <button onClick={logout}>Log Out</button>
