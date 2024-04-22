81ecc40520f9d1ca1204488357e7383adb677e39
Thanh Pham <jonathanphamw22@gmail.com>
2024-04-22 05:16:14 -0700

    1

--- react-vite/src/redux/user.js
+++ react-vite/src/redux/user.js
@@ -105,8 +105,8 @@ export const thunkDeleteSong = (songId) => async dispatch =>{
 
     if(res.ok){
         const deleteSongConfirm = await res.json()
-        dispatch(deleteSong(songId, deleteSongConfirm))
-        dispatch(deleteSongStore(songId, deleteSongConfirm))
+        await dispatch(deleteSong(songId, deleteSongConfirm))
+        await dispatch(deleteSongStore(songId, deleteSongConfirm))
         return deleteSongConfirm
     }
     else{
